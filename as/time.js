ae9b4e498558bad8c03b2cc3aa605fb4ba46a61a
rumengling <37509268+ylyyg@users.noreply.github.com>
2021-08-22 19:14:37 +0800

    Update time.js

--- as/time.js
+++ as/time.js
@@ -20,25 +20,25 @@ function time(){
 	var flag;
 	switch(day){
 		case 1:
-			flag="星期一 / 月曜日";
+			flag="星期一 × 月曜日";
 			break;
 		case 2:
-			flag="星期二 / 火曜日";
+			flag="星期二 × 火曜日";
 			break;
 		case 3:
-			flag="星期三 / 水曜日";
+			flag="星期三 × 水曜日";
 			break;
 		case 4:
-			flag="星期四 / 木曜日";
+			flag="星期四 × 木曜日";
 			break;
 		case 5:
-			flag="星期五 / 金曜日";
+			flag="星期五 × 金曜日";
 			break;
 		case 6:
-			flag="星期六 / 土曜日";
+			flag="星期六 × 土曜日";
 			break;
 		case 0:
-			flag="星期日 / 日曜日";
+			flag="星期日 × 日曜日";
 			break;
 	}
 	document.getElementById("chilk").innerHTML=year+"年"+month+"月"+date+"日"+"&nbsp;"+hh+":"+mm+"&nbsp;"+d+"&nbsp;"+flag;
